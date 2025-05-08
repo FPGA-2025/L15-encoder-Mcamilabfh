b191e2830346137bb05cf74a57ed9c4fec75ef5c
Maria Camila <mcamilabfh@gmail.com>
2025-05-08 00:45:28 -0300

    Tarefa L15 - encoder

--- encoder.v
+++ encoder.v
@@ -1,12 +1,62 @@
+`timescale 1ns/1ps
+
 module Encoder (
-    input wire clk,
-    input wire rst_n,
-
-    input wire horario,
-    input wire antihorario,
-
-    output reg A,
-    output reg B
+    input  wire clk,
+    input  wire rst_n,
+    input  wire horario,
+    input  wire antihorario,
+    output reg  A,
+    output reg  B
 );
-    
+
+    // Quadrantes em Gray code
+    localparam [1:0]
+        Q0 = 2'b00,
+        Q1 = 2'b10,
+        Q2 = 2'b11,
+        Q3 = 2'b01;
+
+    // Estado atual e próximo estado
+    reg [1:0] direcao, next_direcao;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            // Reset inicial
+            direcao      <= Q0;
+            A            <= 1'b0;
+            B            <= 1'b0;
+        end else begin
+            // Calcula o próximo estado (next_direcao)
+            if (horario && !antihorario) begin
+=
+                case (direcao)
+                    Q0: next_direcao = Q1;
+                    Q1: next_direcao = Q2;
+                    Q2: next_direcao = Q3;
+                    Q3: next_direcao = Q0;
+                    default: next_direcao = Q0;
+                endcase
+
+            end else if (!horario && antihorario) begin
+
+                case (direcao)
+                    Q0: next_direcao = Q3;
+                    Q3: next_direcao = Q2;
+                    Q2: next_direcao = Q1;
+                    Q1: next_direcao = Q0;
+                    default: next_direcao = Q0;
+                endcase
+
+            end else begin
+                // Sem pulso: mantém o mesmo quadrante
+                next_direcao = direcao;
+            end
+
+            // Atualiza o estado, separa A e B selecionando a posição 1 para e 0 para vindas do reg next_direção
+            direcao <= next_direcao;
+            A       <= next_direcao[1];
+            B       <= next_direcao[0];
+        end
+    end
+
 endmodule
--- read_encoder.v
+++ read_encoder.v
@@ -6,6 +6,39 @@ module Read_Encoder (
     input wire B,
 
     output reg [1:0] dir
+
 );
+
+    reg A_anterior, B_anterior;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+
+            A_anterior <= 0;
+            B_anterior <= 0;
+            dir <= 2'b00;
+    end else begin
+
+        if ((A_anterior == 0 && B_anterior == 0 && A == 1 && B == 0) ||
+            (A_anterior == 1 && B_anterior == 0 && A == 1 && B == 1) ||
+            (A_anterior == 1 && B_anterior == 1 && A == 0 && B == 1) ||
+            (A_anterior == 0 && B_anterior == 1 && A == 0 && B == 0)) begin
+            dir <= 2'b01; //Sentido horário
     
+        end else if ((A_anterior == 0 && B_anterior == 0 && A == 0 && B == 1) ||
+                 (A_anterior == 0 && B_anterior == 1 && A == 1 && B == 1) ||
+                 (A_anterior == 1 && B_anterior == 1 && A == 1 && B == 0) ||
+                 (A_anterior == 1 && B_anterior == 0 && A == 0 && B == 0)) begin
+        dir <= 2'b10; //Sentido anti-horário
+
+    end else begin
+        dir <= 2'b00;
+    end
+    //Atualizando os estados anteriores
+    A_anterior <= A;
+    B_anterior <= B;
+
+    end
+end
+
 endmodule
